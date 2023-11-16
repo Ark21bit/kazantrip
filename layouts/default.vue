22eba63e49c1cd485e9a1a55d2b5b7fb89ead92d
Ark21bit <webleif@vk.com>
2023-11-16 18:59:31 +0300

    layouts && app

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- <Header></Header> -->
+    <main class="grid-container grow auto-rows-min">
+       <slot></slot>
+    </main>
+    <!-- <Footer/> -->
+ </template> 
---
+++ layouts/main.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- <Header class="absolute w-full z-10"></Header> -->
+    <main class="grid-container grow auto-rows-min">
+       <slot></slot>
+    </main>
+    <!-- <Footer/> -->
+ </template> 
