0dce8a74a91e714273a0cf96b8af415a3072fe7d
Ark21bit <webleif@vk.com>
2023-11-16 18:32:24 +0300

    button component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,91 @@
+<template>
+    <component :is="tag" :class="buttonClass">
+        <slot name="prefix">
+            <Icon v-if="prefixIcon" :name="prefixIcon" :class="iconClass"></Icon>
+        </slot>
+        <slot />
+        <slot name="suffix">
+            <Icon v-if="suffixIconName" :name="suffixIconName" :class="iconClass"></Icon>
+        </slot>
+    </component>
+</template>
+
+<script lang="ts" setup>
+type ButtonColor = 'green'
+type ButtonSize = 'md' | 'sm'
+type ButtonVariant = 'outline' | 'solid'
+const props = defineProps({
+    tag: { type: String, default: 'button' },
+    color: { type: String as PropType<ButtonColor>, default: 'green' },
+    size: { type: String as PropType<ButtonSize>, default: 'md' },
+    variant: { type: String as PropType<ButtonVariant>, default: 'solid' },
+    square: { type: Boolean, default: false },
+    class: { type: [String, Object, Array], default: undefined },
+    prefixIcon: String,
+    suffixIcon: String,
+    loading: Boolean,
+    loadingIcon: { type: String, default: 'line-md:loading-twotone-loop' },
+})
+
+const slots = defineSlots<{
+    default: any,
+    prefix?: any,
+    suffix?: any,
+    loading?: any,
+}>()
+
+const suffixIconName = computed(() => {
+    if (props.loading) return props.loadingIcon
+    return props.suffixIcon
+})
+
+const ui = {
+    color: {
+        green: {
+            solid: 'bg-primary text-white hover:bg-#21747C disabled:bg-inactive',
+            outline: 'bg-transparent ring-1 ring-inactive ring-inset text-diamondBlack hover:(text-primary ring-primary) disabled:(text-inactive ring-bg-inactive)'
+        }
+    },
+    size: {
+        md: 'text-3.75',
+        sm: 'text-sm'
+    },
+    gap: {
+        md: 'gap-2',
+        sm: 'gap-2'
+    },
+    padding: {
+        md: 'px-10 py-3.75',
+        sm: 'px-5.25 py-3.25'
+    },
+    base: 'flex flex-shrink-0 items-center transition-button duration-500 ease-linear',
+    icon: {
+        base: "flex-shrink-0",
+        size: {
+            md: "text-2xl",
+            sm: "text-xl"
+        }
+    },
+    rounded: 'rounded-lg',
+    font: "font-medium"
+}
+
+
+const iconClass = computed(() => {
+    return [ui.icon.base, ui.icon.size[props.size]]
+})
+
+const buttonClass = computed(() => {
+    const variant = ui.color?.[props.color]?.[props.variant]
+    return [
+        `${ui.base} ${!slots?.suffix && !slots?.prefix ? 'justify-center' : 'justify-between'}`,
+        ui.font,
+        ui.rounded,
+        ui.size[props.size],
+        ui.gap[props.size],
+        ui.padding[props.size],
+        variant,
+        props.class
+    ]
+})
+</script>
