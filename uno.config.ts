11214c77c309ef36dfa5f24d8f6caf06071344ce
Ark21bit <webleif@vk.com>
2023-11-16 12:27:37 +0300

    unocss install

---
+++ uno.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from 'unocss'
+/* import formKitUnocssPlugin from "@formkit/themes/unocss" */
+export default defineConfig({
+    presets: [
+        presetUno(),
+        presetWebFonts({
+            fonts: {
+                Onest: { name: "Onest", weights: [400, 500, 600, 700, 800, 900] }
+            },
+        }),
+        /* formKitUnocssPlugin() */
+    ],
+    content: {
+        pipeline: {
+            include: [
+                /\.(vue)($|\?)/,
+                /* './FormkitTheme.js' */
+            ]
+        }
+    },    
+    blocklist: [
+        'container'
+    ],
+    transformers: [
+        transformerDirectives(),
+        transformerVariantGroup()
+    ],
+    theme: {
+        colors: {
+            "primary": "#39919A",
+            "mercury": "#ebebeb",
+            "diamondBlack": "#28303F",
+            "namaraGrey": "#7B7B7B",
+            "second": "#4D4D4D",
+            "lunarBase": "#868686",
+            "inactive": "#C7C7C7",
+            "offBlack": "#303030",
+            "stellarExplorer": "#031F27",
+        },
+    },
+    rules: [
+        [/^leading-([\.\d]+)$/, ([_, num]) => ({ "line-height": `${num}` })]
+    ]
+})
