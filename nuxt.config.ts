1a716005a76e9bb0561e097330084cd37db6d85b
Ark21bit <webleif@vk.com>
2023-11-16 13:33:55 +0300

    nuxt icon install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@unocss/nuxt"],
+    modules: ["@unocss/nuxt", "nuxt-icon"],
     components: [
         { path: "~/components/atoms", pathPrefix: false },
         "~/components/reviews",
