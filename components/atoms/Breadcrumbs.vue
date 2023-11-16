71cf7ff281392120a29c8144f4990223b08364af
Ark21bit <webleif@vk.com>
2023-11-16 18:58:35 +0300

    breadcrumbs create

---
+++ components/atoms/Breadcrumbs.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-diamondBlack text-sm leading-[normal]">
+        <CustomLink v-for="n in 3" to="/test" class="after:(mx-1 content-['/']) last:(text-inactive pointer-events-none after:content-none)">Экскурсии по Татарстану</CustomLink>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    links: Array
+})
+</script>
