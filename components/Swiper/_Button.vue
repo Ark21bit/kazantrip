5c9773d1a7d4fba4c2971b116b83f481f159ed39
Ark21bit <webleif@vk.com>
2023-11-17 15:27:34 +0300

    swiperButton create

---
+++ components/Swiper/_Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button :class="buttonClasses">
+        <slot>
+            <Icon name="Arrow"></Icon>
+        </slot>
+    </button>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+    color: { type: String as PropType<'white' | 'azul'>, default: 'white' }
+})
+const colorClasses = {
+    white: 'bg-white text-diamondBlack hover:(bg-primary text-white) disabled:(bg-inactive text-diamondBlack)',
+    azul: 'bg-#E2F1F2 text-primary hover:(bg-primary text-white) disabled:(bg-inactive text-diamondBlack)'
+}
+
+const buttonClasses = computed(() => [
+    'flex text-2xl rounded-full p-3.25 transition-colors duration-500 ease-linear',
+    colorClasses?.[props.color]
+])
+</script>
