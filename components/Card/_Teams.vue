0340c4200dd6a7ecb8f1c33f4452dc343e74db5e
Ark21bit <webleif@vk.com>
2023-11-17 15:40:52 +0300

    page about create

---
+++ components/Card/_Teams.vue
@@ -0,0 +1,22 @@
+<template>
+    <component :is="tag" class="rounded-5 overflow-hidden card-catalog-shadow">
+        <div class="h-62.25 lg:h-70.25 [&>img]:(w-full h-full object-cover object-center)">
+            <img src="/imgs/team.png" alt="">
+        </div>
+        <div class="rounded-t-5 -mt-5 lg:-mt-10 p-5 bg-white relative ring ring-inset ring-#F6F6F6">
+            <p class="font-semibold text-lg lg:text-xl lg:leading-1.2 leading-1.2 text-diamondBlack">Ксения Алифанова</p>
+            <p class="mt-3 lg:mt-1.5 text-sm text-#A8A8A8 leading-1.2">Генеральный директор</p>
+            <div class="flex gap-1.75 items-center text-primary text-sm leading-1.4 mt-3">
+                <Icon name="Mail" class="text-lg"></Icon> kazantrip@mail.ru
+            </div>
+            <p class="text-diamondBlack text-sm leading-1.4 mt-4">Наш Экскурсионный центр обеспечивал экскурсионное обслуживание участников и гостей XXVII Всемирной летней универсиады студентов 2013 в Казани</p>
+            <CustomLink to="/test" class="flex mt-5 text-primary leading-1.2 text-base underline hover:text-#21747C transition-colors duration-500">Подробнее</CustomLink>
+        </div>
+    </component>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    tag: { type: String, default: 'div' }
+})
+</script>
---
+++ components/Swiper/_Teams.vue
@@ -0,0 +1,67 @@
+<template>
+    <Swiper class="w-full flex relative" :options="options">
+        <CardTeams v-for="item in 10" tag="swiper-slide" class="w-full flex-shrink-0 lg:w-[calc(25%-15px)] last:mr-0 mr-5"></CardTeams>
+        <div slot="container-end" class="flex gap-3 mt-7.5 max-lg:hidden">
+            <SwiperButton color="azul" class="swiper-button-prev"></SwiperButton>
+            <SwiperButton color="azul" class="swiper-button-next rotate-180"></SwiperButton>
+        </div>
+    </Swiper>
+</template>
+
+<script lang="ts" setup>
+import { Mousewheel, Navigation, Pagination } from "swiper/modules";
+import type { SwiperOptions } from 'swiper/types';
+const options: SwiperOptions = {
+    mousewheel: {
+        releaseOnEdges: true,
+        forceToAxis: true
+    },
+    pagination: { clickable: true, enabled: true },
+    breakpoints: {
+        640: { slidesPerView: 2.1 },
+        868: { slidesPerView: 3.1 },
+        1024: {
+            slidesPerView: 4,
+            pagination: { enabled: false },
+            navigation: { enabled: true }
+        }
+    },
+    spaceBetween: 20,
+    slidesPerView: 1.07,
+    navigation: { enabled: false, nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+    modules: [Mousewheel, Navigation, Pagination],
+    injectStyles: [
+        `
+        .swiper {
+            display:flex;
+            overflow:visible;
+            flex-direction:column;
+        }
+        .swiper-pagination {
+            display:flex;
+            gap:8px;
+            margin-top:20px;
+            flex-wrap:wrap;
+            justify-content: center;
+        }
+        .swiper-pagination-bullet {
+            width:10px;
+            height:10px;
+            border-radius:10px;
+            background:#E9E9E9;
+            display:block;
+            transition: background .5s linear;
+            flex-shrink:0;
+            cursor:pointer;
+        } 
+        .swiper-pagination.swiper-pagination-disabled {
+            display:none;
+        } 
+        .swiper-pagination-bullet-active  {           
+            background:#39919A;
+        } 
+      `,
+    ],
+}
+</script>
+
