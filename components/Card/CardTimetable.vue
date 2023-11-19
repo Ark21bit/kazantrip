487afc2a816560311a04aa05962ff2361a04f28d
Ark21bit <webleif@vk.com>
2023-11-19 15:43:43 +0300

    cardTimetable create && use

---
+++ components/Card/CardTimetable.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col rounded-5 overflow-hidden border border-#EBEBEB lg:border-transparent shadow-base">
+        <div class="bg-#E2F1F2 p-5 text-base lg:text-lg font-semibold leading-1.2 lg:leading-1.2 text-diamondBlack border-b border-#EBEBEB">
+            <p>Расписание на</p>
+            <p class="text-primary">четверг 31 октября</p>
+        </div>
+        <div v-for="n in 3" class="flex flex-col gap-2 py-4.5 p-5 border-b border-#EBEBEB text-sm lg:text-base font-semibold leading-1.2 lg:leading-1.2">
+            <p class="leading-1.4 text-#7B7B7B">11:00</p>
+            <p class="text-diamondBlack">Остров-град Свияжск и Храм всех религий (внешний осмотр)</p>
+            <p class="text-primary text-xl">1999₽<span class="ml-1.5 py-1.5 text-sm text-#A4A4A4 line-through decoration-diamondBlack">1350₽</span></p>
+        </div>
+        <CustomLink to="/test" class="text-#39919A underline leading-1.2 text-base hover:text-#21747C mx-auto my-5">Еще экскурсии</CustomLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- pages/timetable.vue
+++ pages/timetable.vue
@@ -6,18 +6,7 @@
     </div>
     <div class="col-span-full pt-7.5 border-t border-#EBEBEB mt-7.5">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7.5 wrapper">
-            <div v-for="n in 4" class="flex flex-col rounded-5 overflow-hidden border border-#EBEBEB lg:border-transparent card-timetable-shadow">
-                <div class="bg-#E2F1F2 p-5 text-base lg:text-lg font-semibold leading-1.2 lg:leading-1.2 text-diamondBlack border-b border-#EBEBEB">
-                    <p>Расписание на</p>
-                    <p class="text-primary">четверг 31 октября</p>
-                </div>
-                <div v-for="n in 3" class="flex flex-col gap-2 py-4.5 p-5 border-b border-#EBEBEB text-sm lg:text-base font-semibold leading-1.2 lg:leading-1.2">
-                    <p class="leading-1.4 text-#7B7B7B">11:00</p>
-                    <p class="text-diamondBlack">Остров-град Свияжск и Храм всех религий (внешний осмотр)</p>
-                    <p class="text-primary text-xl">1999₽<span class="ml-1.5 py-1.5 text-sm text-#A4A4A4 line-through decoration-diamondBlack">1350₽</span></p>
-                </div>
-                <CustomLink to="/test" class="text-#39919A underline leading-1.2 text-base hover:text-#21747C mx-auto my-5">Еще экскурсии</CustomLink>
-            </div>
+            <CardTimetable v-for="n in 4"></CardTimetable>
         </div>
     </div>
 </template>
