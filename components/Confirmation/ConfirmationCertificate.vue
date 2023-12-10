2533e6e688a4dbdb611524b2951fb775bb5f5ce5
Ark21bit <webleif@vk.com>
2023-12-11 01:57:01 +0300

    fix

--- components/Confirmation/ConfirmationCertificate.vue
+++ components/Confirmation/ConfirmationCertificate.vue
@@ -2,19 +2,19 @@
     <slot name="title"></slot>
     <div class="mt-7.5 lg:mt-10 shadow-base rounded-5 ring ring-#F6F6F6 ring-inset lg:py-2.5">
         <slot name="head"></slot>
-        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-diamondBlack">
+        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-fblack">
             <p>{{ 'название сертификата' }}</p>
             <p class="font-medium"> {{ orderInfo?.title }}</p>
         </div>
-        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-diamondBlack">
+        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-fblack">
             <p>{{ generalConfig?.static_info?.global_words?.product_name }}</p>
             <p class="font-medium">{{ orderInfo?.certificateTitle }}</p>
         </div>
-        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-diamondBlack">
+        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-fblack">
             <p>{{ generalConfig?.static_info?.global_words?.tourist_telephone }}</p>
             <p class="font-medium">{{ orderInfo?.client_telephone }}</p>
         </div>
-        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-diamondBlack">
+        <div class="flex flex-col lg:flex-row justify-between gap-3 py-3 px-5 lg:px-7.5 last:border-none border-b border-#E8E8E8 text-sm leading-1.4 text-fblack">
             <p>{{ generalConfig?.static_info?.global_words?.email }}</p>
             <p class="font-medium">{{ orderInfo?.client_email }}</p>
         </div>
