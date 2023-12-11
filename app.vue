576b5b52b5234baa2aab7b089b00b2768bc75ce4
Ark21bit <webleif@vk.com>
2023-12-11 12:35:10 +0300

    socials fix

--- app.vue
+++ app.vue
@@ -4,10 +4,10 @@
     </NuxtLayout>
     <div id="teleported"></div>
     <div class="fixed z-30 right-[var(--width-content-margin)] bottom-7.5 leading-none items-end flex flex-col gap-2 text-white group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
-        <CustomLink target="_blank" to="/test" class="p-2.5 bg-#2AABEE transition-colors duration-500 ease-linear rounded-lg">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.telegram" class="p-2.5 bg-#2AABEE transition-colors duration-500 ease-linear rounded-lg">
             <Icon size="24" name="Tg"></Icon>
         </CustomLink>
-        <CustomLink target="_blank" to="/test" class="p-2.75 bg-#60D669 transition-colors duration-500 ease-linear rounded-lg">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.whatsApp" class="p-2.75 bg-#60D669 transition-colors duration-500 ease-linear rounded-lg">
             <Icon size="22" name="Whatsapp"></Icon>
         </CustomLink>
         <NuxtLink target="_blank" to="/test" class="py-3 px-4.125 bg-primary transition-colors duration-500 ease-linear rounded-lg text-base font-medium">
--- components/Socials.vue
+++ components/Socials.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <CustomLink :to="generalConfig?.static_info?.contact?.socials?.telegram">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.telegram">
             <Icon name="Tg" class="hover:text-#39919A transition-colors"></Icon>
         </CustomLink>
-        <CustomLink :to="generalConfig?.static_info?.contact?.socials?.whatsApp">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.whatsApp">
             <Icon name="Whatsapp" class="hover:text-#39919A transition-colors"></Icon>
         </CustomLink>
-        <CustomLink :to="generalConfig?.static_info?.contact?.socials?.vk">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.vk">
             <Icon name="Vk" class="hover:text-#39919A transition-colors"></Icon>
         </CustomLink>
     </div>
