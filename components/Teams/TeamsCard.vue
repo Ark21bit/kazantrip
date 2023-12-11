617a1db2dc388c0fa9d9acc2ccd7308494d153c2
Ark21bit <webleif@vk.com>
2023-12-11 12:35:53 +0300

    teamsCard fix

--- components/Teams/TeamsCard.vue
+++ components/Teams/TeamsCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <component :is="tag" class="rounded-5 overflow-hidden shadow-base">
+    <div class="rounded-5 overflow-hidden shadow-base">
         <div class="h-62.25 lg:h-70.25 [&>img]:(w-full h-full object-cover object-center)" v-html="media_avatar"></div>
         <div class="rounded-t-5 -mt-5 lg:-mt-10 p-5 bg-white relative ring ring-inset ring-#F6F6F6">
             <p class="font-semibold text-lg lg:text-xl lg:leading-1.2 leading-1.2 text-fblack">{{ fio }}</p>
@@ -10,13 +10,13 @@
             <!-- <p class="text-fblack text-sm leading-1.4 mt-4">{{ description }}</p> -->
             <Button class="mt-5" padded size="lg" variant="link" @click="emit('openTeamFull')">Подробнее</Button>
         </div>
-    </component>
+    </div>
 </template>
 
 <script setup lang="ts">
 import type { TeamsDatum } from '~/types/fetch/about';
 
-withDefaults(defineProps<TeamsDatum & { tag: string | object }>(), { tag: 'div' })
+defineProps<TeamsDatum>()
 
 const emit = defineEmits(['openTeamFull'])
 </script>
