746f1a39d8fda4eb07d458a794d751013431c840
Ark21bit <webleif@vk.com>
2023-12-11 12:35:32 +0300

    Modalthanks fix

--- components/Modal/ModalThanksContent.vue
+++ components/Modal/ModalThanksContent.vue
@@ -1,8 +1,8 @@
 <template>
-    <img class="w-49 h-24 mt-2.5" src="/thanksBg.png" alt="">
+    <img class="w-49 h-24 mt-2.5 mx-auto" src="/thanksBg.png" alt="">
     <h2 class="mt-7.5 lg:mt-5 text-center font-Montserrat font-bold leading-1.2 sm:leading-1.2 text-5.5 sm:text-6.5 text-fblack">{{ title }}</h2>
     <p class="mt-3 text-center text-second text-sm leading-1.4">{{ subTitle }}</p>
-    <Button :tag="CustomLink" to="/" class="mt-6 w-full lg:w-49 mx-auto"> Ок </Button>
+    <Button @click="emit('ok')" class="mt-6 w-full lg:w-49 mx-auto"> Ок </Button>
 </template>
 
 <script lang="ts" setup>
@@ -11,4 +11,6 @@ defineProps({
     title: String,
     subTitle: String,
 })
+
+const emit = defineEmits(['ok'])
 </script>
