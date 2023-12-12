451acbd0fc27bc0cf192bcac88dbb17adcb1d4a7
Ark21bit <webleif@vk.com>
2023-12-12 14:20:43 +0300

    OrderCertificate

--- components/Pages/PagesCatalog.vue
+++ components/Pages/PagesCatalog.vue
@@ -4,18 +4,51 @@
     <div class="mt-2.5 col-span-full border-t lg:border-y border-#EBEBEB pt-7.5 lg:py-7.5">
         <CatalogContainer class="wrapper" :timetable="true">
             <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-5 gap-y-5 lg:gap-y-7.5 h-fit max-lg:(pb-7.5 border-b border-#EBEBEB)">
-                <CardCatalog v-for="card in pageInfo?.info_products?.data" :type_id="card?.type_id" :img="card?.media_preview" :slug="card?.slug" :title="card?.lang_info?.title" :duration="card?.duration_event" :price="card?.price_see" :description="card?.lang_info?.description"
-                    :reviews-count="card.reviews_count" :is-sale="card?.is_sale" :category_id="card?.category_id" :is-radio-gid="card?.is_radio_gid" :price-old="card?.price_see_old" :rating="card?.rating"></CardCatalog>
+                <CardCatalog @order="openOrder(card?.slug)" v-for="card in pageInfo?.info_products?.data" :type_id="card?.type_id" :img="card?.media_preview" :slug="card?.slug" :title="card?.lang_info?.title" :duration="card?.duration_event" :price="card?.price_see" :description="card?.lang_info?.description"
+                    :reviews-count="card.reviews_count" :is-sale="card?.is_sale" :category_id="card?.category_id" :is-radio-gid="card?.is_radio_gid" :price-old="card?.price_see_old" :rating="card?.rating" />
             </div>
         </CatalogContainer>
     </div>
     <div v-if="pageInfo?.info_seo_blocks?.data?.length ?? 0 > 0" class="mt-20 lg:mt-25 pb-7.5 border-b border-#EBEBEB col-span-full">
-        <SeoBlock v-for="item in pageInfo?.info_seo_blocks?.data" :title="item?.lang_info?.title" :text="item?.lang_info?.text" class="wrapper"></SeoBlock>
+        <SeoBlock v-for="item in pageInfo?.info_seo_blocks?.data" :title="item?.lang_info?.title" :text="item?.lang_info?.text" class="wrapper" />
     </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-    pageInfo: Object
+    pageInfo: Object,
+    isSearched: Boolean,
 })
+
+const { generalConfig } = storeToRefs(useGeneralConfigStore())
+
+const localePath = useLocalePath()
+const route = useRoute()
+
+const selectOrder = ref<string | null>(null)
+const { data: product, pending, execute } = useBaseFetch('search/slugs', {
+    immediate: false,
+    query: { slug: selectOrder },
+    transform: (product: any) => product.data,
+    watch: false
+})
+
+const order = computedWithControl(() => product.value?.type_id, () => {
+    if (product.value?.type_id == 11) return resolveComponent('OrderRegular')
+    if (product.value?.type_id == 12) return resolveComponent('OrderIndividual')
+    if (product.value?.type_id == 31) return resolveComponent('OrderCertificate')
+    return null
+})
+
+
+const dayjs = useDayjs()
+const dateVisible = computed(() => route.path == localePath('/ekskursii/regulyarnyye'))
+const changeTimetamble = async (date: any) => await navigateTo(localePath(`/excursion/timetable/${dayjs(date).format('YYYY/MM/DD')}`))
+
+const { closeModal, isModalShow, openModal } = useModal()
+const openOrder = async (value: string) => {
+    openModal()
+    selectOrder.value = value
+    execute()
+}
 </script>
