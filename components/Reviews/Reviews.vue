6eaa8c25ce76ed4625a23bda1334e06c22ad2c85
Ark21bit <webleif@vk.com>
2023-12-12 16:12:39 +0300

    reviews fix

--- components/Reviews/Reviews.vue
+++ components/Reviews/Reviews.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="flex flex-col lg:flex-row justify-between gap-4 lg:items-center">
             <h2 class="text-fblack text-5.5 lg:text-7 font-bold leading-1.2 lg:leading-1.2 font-Montserrat">{{ generalConfig?.static_info?.global_words?.reviews }}</h2>
-            <ReviewsModalCreate #="{ openModal }">
+            <ReviewsModalCreate :product-id="productId" :product-title="title" #="{ openModal }">
                 <Button @click="openModal" class="ml-auto">{{ generalConfig?.static_info?.global_words?.add_review }}</Button>
             </ReviewsModalCreate>
         </div>
