61e4551f3a2406a5112c6d80be337c15415c76f3
Ark21bit <webleif@vk.com>
2023-12-14 11:46:20 +0300

    duration text fix

--- components/IndexProducts.vue
+++ components/IndexProducts.vue
@@ -2,7 +2,7 @@
     <div v-bind="$attrs" class="flex flex-col gap-7.5 lg:gap-10">
         <h2 class="font-Montserrat text-5.5 lg:text-8 leading-1.2 lg:leading-1.2 font-bold text-fblack">{{ title }}</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-y-7.5">
-            <CardCatalog v-for="card in products" @order="openOrder(card?.slug)" :type_id="card?.type_id" :img="card?.media_preview" :slug="card?.slug" :title="card?.lang_info?.title" :duration="card?.duration_event" :price="card?.price_see" :description="card?.lang_info?.description"
+            <CardCatalog v-for="card in products" @order="openOrder(card?.slug)" :type_id="card?.type_id" :img="card?.media_preview" :slug="card?.slug" :title="card?.lang_info?.title" :duration="card?.duration_event_text" :price="card?.price_see" :description="card?.lang_info?.description"
                 :reviews-count="card.reviews_count" :is-sale="card?.is_sale" :category_id="card?.category_id" :is-radio-gid="card?.is_radio_gid" :price-old="card?.price_see_old" :rating="card?.rating"></CardCatalog>
             <Button :tag="CustomLink" to="/ekskursii" class="col-span-full lg:mx-auto mt-2.5 lg:mt-0">{{ generalConfig?.static_info?.global_words?.more_excursion }}</Button>
         </div>
