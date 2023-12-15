e8f001211e4811587cfe628b87a521173a90ea0f
Ark21bit <webleif@vk.com>
2023-12-15 12:16:50 +0300

    sliders fix

--- components/Slider/SliderRecomendations.vue
+++ components/Slider/SliderRecomendations.vue
@@ -3,7 +3,7 @@
         <SwiperSlide class="lg:w-[calc(50%-10px)] last:mr-0 mr-5 h-auto" v-for="item in data">
             <CardRecomendation :img="item?.data?.media_preview" :slug="item?.data?.slug" :is-sale="item?.data?.is_sale" :title="item?.data?.lang_info?.title" :price-old="item?.data?.price_see_old" :price="item?.data?.price_see" class="h-full" />
         </SwiperSlide>
-        <SliderController color="azul" class="mt-7.5 max-lg:hidden w-fit" />
+        <SliderController color="azul" class="absolute max-lg:hidden top-1/2 -translate-y-1/2 w-full z-1" />
         <SliderPagination color="gray" class="mt-5 lg:hidden" />
     </Swiper>
 </template>
