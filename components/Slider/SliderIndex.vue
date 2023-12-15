a93d58faa1f56a9452ca48e6467099afb71850b7
Ark21bit <webleif@vk.com>
2023-12-15 16:10:03 +0300

    slider index fix

--- components/Slider/SliderIndex.vue
+++ components/Slider/SliderIndex.vue
@@ -4,9 +4,9 @@
             <div class="absolute inset-0">
                 <img src="/sliderBg2.svg" class="w-full h-full object-cover object-bottom" alt="">
             </div>
-            <div class="absolute inset-0 index__slider-img-container [&>img]:(w-full h-full object-cover object-bottom)">
+            <div class="absolute inset-0 index__slider-img-container">
                 <div class="absolute top-0 left-0 w-full h-full bg-black/50"></div>
-                <div v-html="slide?.media_preview"></div>
+                <div class="w-full h-full [&>img]:(w-full h-full object-cover object-bottom)" v-html="slide?.media_preview"></div>
             </div>
             <div class="flex flex-col items-center gap-4 lg:gap-5 text-white z-1 wrapper">
                 <h2 class="font-Montserrat text-6.5 lg:text-10.5 leading-1.2 lg:leading-1.2 font-bold text-center lg:w-[calc(100%-6.25rem)] lg:max-w-266">{{ slide?.lang_info?.title }}</h2>
