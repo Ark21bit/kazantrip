323289215d311f234125bf74e46ee6b6c9614feb
Ark21bit <webleif@vk.com>
2023-12-18 00:07:05 +0300

    about fix

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,6 @@
 <template>
     <h1 class="font-Montserrat text-6.5 lg:text-9.5 leading-1.2 font-bold text-fblack mt-10 lg:mt-20">{{ pageInfo?.content?.title }}</h1>
-    <p class="mt-5 lg:mt-7.5 font-semibold leading-1.4 text-base lg:text-xl max-w-211.75 text-fblack [&>span]:text-primary">
-        Экскурсионный центр города Казани открылся в <span>2010</span> году и был <span>первым</span>, кто стал проводить сборные ежедневные экскурсии по <span>Казани</span> и <span>Татарстану</span>.
-    </p>
+    <p class="mt-5 lg:mt-7.5 font-semibold leading-1.4 text-base lg:text-xl max-w-211.75 text-fblack [&>span]:text-primary">{{ pageInfo?.content?.text_1 }}</p>
     <AboutHistory :data="pageInfo?.history_year"/>
     <div class="mt-20 lg:mt-37.5 col-span-full grid-container overflow-hidden">
         <h2 class="text-fblack text-5.5 lg:text-7 font-bold leading-1.2 lg:leading-1.2 font-Montserrat [&>span]:text-primary">Наша команда <span>менеджеров</span></h2>
