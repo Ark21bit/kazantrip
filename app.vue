462d42b31f6f81247ea26510e122f5ba712d1d3c
Ark21bit <webleif@vk.com>
2023-12-18 00:04:19 +0300

    uno css fix

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
     </main>
     <Footer />
     <div id="teleported"></div>
-    <div class="fixed z-30 right-[var(--width-content-margin)] bottom-7.5 leading-none items-end flex flex-col gap-2 text-white group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
+    <div class="fixed z-30 right-[--width-content-margin] bottom-7.5 leading-none items-end flex flex-col gap-2 text-white group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
         <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.telegram" class="p-2.5 bg-#2AABEE transition-colors duration-500 ease-linear rounded-lg">
             <Icon size="24" name="Tg"></Icon>
         </CustomLink>
