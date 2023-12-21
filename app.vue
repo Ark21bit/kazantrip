aea571418e0a97e705b9c6259b523f0917b31599
Ark21bit <webleif@vk.com>
2023-12-21 23:16:49 +0300

    up button fix

--- app.vue
+++ app.vue
@@ -6,8 +6,9 @@
     <Footer />
     <div id="teleported"></div>
     <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" enter-active-class="transition-opacity duration-300 ease-in-out" leave-active-class="transition-opacity duration-300 ease-in">
-        <button v-show="scrollToUpButtonVisible" @click="scrollToUp" class="fixed top-1/2 -translate-y-1/2 right-[--width-content-margin] z-30 text-white flex items-center gap-2.5 text-sm leading-1.25 max-lg:hidden group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
-            <Icon name="ArrowUp" class="text-11 p-2.5 bg-#39919A rounded-full"></Icon>
+        <button v-show="scrollToUpButtonVisible" @click="scrollToUp" class="text-primary fixed top-1/2 -translate-y-1/2 right-[--width-content-margin] z-30 flex items-center gap-2.5 text-sm leading-1.25 group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
+            <span class="max-lg:hidden">{{ generalConfig?.static_info?.global_words?.up }}</span>
+            <Icon name="ArrowUp" class="text-11 p-2.5 text-white bg-#39919A rounded-full"></Icon>
         </button>
     </Transition>
     <div class="fixed z-30 right-[--width-content-margin] bottom-7.5 leading-none items-end flex flex-col gap-2 text-white group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
@@ -53,7 +54,6 @@ const scrollToUpButtonVisible = computed(() => {
 const scrollToUp = () => {
     window.scrollTo({ left: 0, top: 0, behavior: 'smooth' })
 }
-
 
 const route = useRoute()
 const headerAttrs = computed(() => ({ ...route.meta?.headerAttrs as object }))
