7a9921fa471fefa623e1c5a151f65f0849b16647
Ark21bit <webleif@vk.com>
2023-12-21 23:18:00 +0300

    default img fix

--- utils/getDefaultImg.ts
+++ utils/getDefaultImg.ts
@@ -1,3 +1,3 @@
 export const getDefaultImg = () => {    
-    return '<img src="/imgs/logo.svg" alt="" style="object-fit:contain;object-position:center">'
+    return '<img src="/imgs/logo.svg" alt="" style="object-fit:contain;object-position:center;padding:20px">'
 };
