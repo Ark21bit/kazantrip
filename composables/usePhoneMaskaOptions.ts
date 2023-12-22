6da5c3dbec28d299896aa315a9402cdac1cb4ce9
Ark21bit <webleif@vk.com>
2023-12-22 18:39:09 +0300

    maska fix

--- composables/usePhoneMaskaOptions.ts
+++ composables/usePhoneMaskaOptions.ts
@@ -2,8 +2,8 @@ export const usePhoneMaskaOptions = () => {
     const options = {
         mask: (value: string) => {
             if (value.startsWith('8')) return '8(###)###-##-##'
-            if (value.startsWith('7')) return '+7(###)###-##-##'
-            if (value.startsWith('+7')) return '+7(###)###-##-##'
+            if (value.startsWith('+7') || value.startsWith('7')) return '+7(###)###-##-##'
+            if (value.startsWith('+375') || value.startsWith('375')) return '+375(##)###-##-##'
             return null
         }
     }
