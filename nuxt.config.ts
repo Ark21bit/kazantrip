7971f74494e1c8563033cd3a94a82734feee85db
Ark21bit <webleif@vk.com>
2023-12-23 00:57:42 +0300

    favicon add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,14 @@ export default defineNuxtConfig({
         transpile: ['@vuepic/vue-datepicker']
     },
     app: {
+        link: [
+            { rel: "shortcut icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png" },
+            { rel: "icon", type: "image/png", sizes: "512x512", href: "/favicons/512.png" },
+            { rel: "icon", type: "image/png", sizes: "192x192", href: "/favicons/192.png" },
+            { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/32.png" },
+            { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/16.png" }
+        ],
         head: {
             bodyAttrs: {
                 class: 'group/body'
