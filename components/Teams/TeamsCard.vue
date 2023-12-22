7cb15b38bf89952238048e4bdd560c6d12d3b38c
Ark21bit <webleif@vk.com>
2023-12-22 12:47:59 +0300

    teams card add deafult img

--- components/Teams/TeamsCard.vue
+++ components/Teams/TeamsCard.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="rounded-5 overflow-hidden shadow-base">
-        <div class="h-62.25 lg:h-70.25 [&>img]:(w-full h-full object-cover object-center)" v-html="media_avatar"></div>
+        <div class="h-62.25 lg:h-70.25 [&>img]:(w-full h-full object-cover object-center)" v-html="media_avatar ?? `<img src='/imgs/avatar.svg' style='padding:20px 20px 0;object-fit:contain' alt=''>`">
+        </div>
         <div class="rounded-t-5 -mt-5 lg:-mt-10 p-5 bg-white relative ring ring-inset ring-#F6F6F6">
             <p class="font-semibold text-lg lg:text-xl lg:leading-1.2 leading-1.2 text-fblack">{{ fio }}</p>
             <p class="mt-3 lg:mt-1.5 text-sm text-#A8A8A8 leading-1.2">{{ post }}</p>
-             <NuxtLink v-if="email" :to="`mailto:${email}`" class="flex gap-1.75 items-center text-primary text-sm leading-1.4 mt-3">
+            <NuxtLink v-if="email" :to="`mailto:${email}`" class="flex gap-1.75 items-center text-primary text-sm leading-1.4 mt-3">
                 <Icon name="Mail" class="text-lg"></Icon>{{ email }}
             </NuxtLink>
             <p class="text-fblack text-sm leading-1.4 mt-4">{{ about }}</p>
