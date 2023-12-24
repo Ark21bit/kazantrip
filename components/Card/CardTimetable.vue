00c70750dd3b82bb08fdaa90d468bc1d811f89a6
Ark21bit <webleif@vk.com>
2023-12-25 01:34:20 +0300

    timetable styles fix

--- components/Card/CardTimetable.vue
+++ components/Card/CardTimetable.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col rounded-5 overflow-hidden border border-#EBEBEB lg:border-transparent shadow-base">
-        <div class="flex bg-#E2F1F2 p-5 gap-3 text-base lg:text-lg font-semibold leading-1.2 lg:leading-1.2 text-fblack border-b border-#EBEBEB  justify-between">
+    <div class="flex flex-col rounded-5 lg:rounded-3 3xl:rounded-5 overflow-hidden border border-#EBEBEB lg:border-transparent shadow-base">
+        <div class="flex bg-#E2F1F2 p-5 lg:p-3 3xl:p-5 gap-3 text-base lg:text-lg font-semibold leading-1.2 lg:leading-1.2 text-fblack border-b border-#EBEBEB  justify-between">
             <div>
                 <p>{{ generalConfig?.static_info?.global_words?.timetable_on }}</p>
                 <p class="text-primary">{{ $dayjs(day).tz('Europe/Moscow').locale(locale).format('dddd DD MMMM') }}</p>
@@ -9,7 +9,7 @@
         </div>
         <div>
             <template v-if="info">
-                <div v-for="item in isShort ? infoShorts : info" class="flex flex-col gap-2 py-4.5 p-5 first:border-none border-t border-#EBEBEB text-sm lg:text-base font-semibold leading-1.2 lg:leading-1.2">
+                <div v-for="item in isShort ? infoShorts : info" class="flex flex-col gap-2 lg:gap-1 3xl:gap-2 py-4.5 lg:py-2 3xl:py-4.5 p-5 lg:p-3 3xl:p-5 first:border-none border-t border-#EBEBEB text-sm lg:text-base font-semibold leading-1.2 lg:leading-1.2">
                     <p class="leading-1.4 text-#7B7B7B">{{ item?.time }}</p>
                     <CustomLink :to="item?.url" class="text-fblack">{{ item?.product_name }}</CustomLink>
                     <p class="text-primary text-xl">{{ item?.visible_prices?.new }}₽<span class="ml-1.5 py-1.5 text-sm text-#A4A4A4 line-through decoration-fblack" v-if="item?.visible_prices?.old">{{ item?.visible_prices?.old }}₽</span></p>
@@ -19,7 +19,7 @@
                 <Icon class="text-5xl text-primary" name="eos-icons:loading"></Icon>
             </div>
         </div>
-        <div v-if="Number(info?.length) > 3" class="mt-auto flex py-5 border-t border-#EBEBEB">
+        <div v-if="Number(info?.length) > 3" class="mt-auto flex py-5 lg:py-3 3xl:py-5 border-t border-#EBEBEB">
             <Button @click="toggle" variant="link" size="lg" padded class="mx-auto">{{ isShort ? generalConfig?.static_info?.global_words?.more_excursions : generalConfig?.static_info?.global_words?.hide_more_details }}</Button>
         </div>
     </div>
--- pages/timetable.vue
+++ pages/timetable.vue
@@ -1,10 +1,10 @@
 <template>
-    <h1 class="font-Montserrat text-6.5 lg:text-9.5 leading-1.2 font-bold text-fblack mt-10 lg:mt-20">{{ pageInfo?.content?.title }}</h1>
-    <div class="flex flex-col md:flex-row gap-3 lg:gap-1.75 mt-5 lg:mt-7.5">
+    <h1 class="font-Montserrat text-6.5 lg:text-9.5 leading-1.2 font-bold text-fblack mt-10 lg:mt-5 3xl:mt-20">{{ pageInfo?.content?.title }}</h1>
+    <div class="flex flex-col md:flex-row gap-3 lg:gap-1.75 mt-5 3xl:mt-7.5">
         <Bage v-for="file in pageInfo?.files" href="#" :download="file?.url" tag="a" link class="max-lg:py-1.5 max-lg:px-3">{{ file?.title }}</Bage>
     </div>
-    <div class="col-span-full pt-7.5 border-t border-#EBEBEB mt-7.5 lg:mb-12.5">
-        <div class="flex flex-wrap gap-7.5 justify-center wrapper">
+    <div class="col-span-full pt-7.5 lg:pt-4 3xl:pt-7.5 border-t border-#EBEBEB mt-7.5 lg:mt-4 3xl:mt-7.5 lg:mb-12.5">
+        <div class="flex flex-wrap gap-7.5 lg:gap-3 3xl:gap-7.5 justify-center wrapper">
             <CardTimetable class="w-full sm:w-[calc(50%-15px)] lg:w-[calc(100%/3-20px)] xl:w-[calc(25%-22.5px)]" v-for="item in pageInfo?.timetable" :info="item?.info" :day="item?.day" />
         </div>
     </div>
