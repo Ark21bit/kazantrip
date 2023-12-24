f9771811c2f56d556c1aa1bcea8e4f1f65d25e30
Ark21bit <webleif@vk.com>
2023-12-25 01:34:30 +0300

    order individual fix

--- components/Order/OrderIndividual.vue
+++ components/Order/OrderIndividual.vue
@@ -1,7 +1,7 @@
 <template>
-    <FormKit @submit="setOrder" v-model="forms" type="form" form-class="flex flex-col gap-5 lg:gap-6 lg:mt-1" :actions="false">
+    <FormKit @submit="setOrder" v-model="forms" type="form" form-class="flex flex-col gap-5 lg:gap-3 3xl:gap-6 lg:mt-1" :actions="false">
         <p class="text-sm lg:text-base font-medium leading-1.4 lg:leading-1.4 text-primary mt-1 lg:mt-0">{{ product?.lang_info?.title }}</p>
-        <div class="flex flex-col gap-4.5 lg:gap-5">
+        <div class="flex flex-col gap-4.5 lg:gap-3 3xl:gap-5">
             <h4 class="text-base font-semibold text-fblack leading-1.2 font-Montserrat">{{ generalConfig?.static_info?.global_words?.select_datetime }}</h4>
             <FormKit name="date" validation="required" type="datepickerC" :minDate="$dayjs().tz('Europe/Moscow').toDate()" :validation-label="generalConfig?.static_info?.global_words?.date" :placeholder="generalConfig?.static_info?.global_words?.date"></FormKit>
         </div>
@@ -11,19 +11,19 @@
                 <AdditionalsTable :tickets="product?.info_additional_products?.data"></AdditionalsTable>
             </FormKit>
         </div>
-        <div class="flex flex-col gap-4.5 lg:gap-5">
+        <div class="flex flex-col gap-4.5 lg:gap-3 3xl:gap-5">
             <h4 class="text-base font-semibold text-fblack leading-1.2 font-Montserrat">{{ generalConfig?.static_info?.global_words?.comment_for_order }}</h4>
             <FormKit name="comment_client" validation="length:0,2000" type="textarea" :validation-label="generalConfig?.static_info?.global_words?.comment_for_order"></FormKit>
         </div>
-        <div class="flex flex-col gap-4 lg:gap-6 max-lg:mt-2.5">
+        <div class="flex flex-col gap-4 lg:gap-3 3xl:gap-5 max-lg:mt-2.5">
             <h4 class="text-base font-semibold text-fblack leading-1.2 font-Montserrat">{{ generalConfig?.static_info?.global_words?.person_data_contract }}</h4>
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-4.5">
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-3 3xl:gap-4.5">
                 <FormKit name="client_fio" validation="required:trim|length:3,64" type="text" :validation-label="generalConfig?.static_info?.global_words?.fio" :placeholder="generalConfig?.static_info?.global_words?.fio" />
                 <FormKit name="type_price_id" validation="required:trim" type="selectC" :options="typePrices" :validation-label="generalConfig?.static_info?.global_words?.count_people" :placeholder="generalConfig?.static_info?.global_words?.count_people"></FormKit>
                 <FormKit name="client_telephone" :validation="[['matches', /^[0-9\(\)\-\+\s]+$/], ['required', 'trim']]" v-maska:[options] type="tel" :validation-label="generalConfig?.static_info?.global_words?.telephone" :placeholder="generalConfig?.static_info?.global_words?.telephone" />
                 <FormKit name="payment_type_id" validation="required" type="selectC" :options="paymentTypes" :validation-label="generalConfig?.static_info?.global_words?.type_payment" :placeholder="generalConfig?.static_info?.global_words?.type_payment" />
                 <FormKit name="client_email" validation="email|required|length:0,32" type="email" :validation-label="generalConfig?.static_info?.global_words?.email" :placeholder="generalConfig?.static_info?.global_words?.email" />
-                <div class="flex flex-col gap-4 col-span-full">
+                <div class="flex flex-col gap-4 lg:gap-2 3xl:gap-4 col-span-full">
                     <FormKit :validation-messages="{ accepted: String(generalConfig?.static_info?.global_words?.confirm_excursion_info) }" validation="accepted" type="checkbox" label-class="[&_a]:text-primary [&_a]:hover:text-#21747C">
                         <template #label="{ classes }">
                             <span :class="classes.label" v-html="generalConfig?.static_info?.global_words?.order_confirm_procedure_provision_excursion_services"></span>
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-col lg:flex-row gap-3 lg:justify-between mt-2.5 lg:mt-1.5">
+        <div class="flex flex-col lg:flex-row gap-3 lg:gap-2 3xl:gap-3 lg:justify-between mt-2.5 lg:mt-0 3xl:mt-1.5">
             <Button @click="emit('close')" class="w-full lg:w-49" type="button" variant="outline">{{ generalConfig?.static_info?.global_words?.cancel }}</Button>
             <Button class="w-full lg:w-49">{{ generalConfig?.static_info?.global_words?.reservation }}</Button>
         </div>
