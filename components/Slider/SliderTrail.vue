421ec01913d5146b85e8cd5a25133fb1d9bf3fb2
Ark21bit <webleif@vk.com>
2023-12-25 14:36:53 +0300

    slider trail fix

--- components/Slider/SliderTrail.vue
+++ components/Slider/SliderTrail.vue
@@ -1,13 +1,13 @@
 <template>
     <Swiper slidesPerView="auto" class="w-full flex flex-col relative contain-layout overflow-visible" v-bind="options">
-        <SwiperSlide class="w-full w-62.5 lg:w-76.25 h-full shrink-0 [&>img]:(w-full h-full object-cover object-center) w-9/10 max-w-80 lg:max-w-auto sm:w-[calc(50%-10px)] lg:w-[calc(25%-20px/3)] group/swiperTrail" v-for="(trail, index) in trails">
+        <SwiperSlide class="flex flex-col w-full w-62.5 lg:w-76.25 h-auto shrink-0 [&>img]:(w-full h-full object-cover object-center) w-9/10 max-w-80 lg:max-w-auto sm:w-[calc(50%-10px)] lg:w-[calc(25%-20px/3)] group/swiperTrail" v-for="(trail, index) in trails">
             <div class="relative translate-x-3.25">
                 <div class="ring-(1 inset #F6F6F6) relative z-1 w-fit bg-white rounded-full p-1.25 after-swiper-trail">
                     <span class="w-8 aspect-square rounded-full bg-primary group-last/swiperTrail:bg-red flex items-center justify-center text-white text-lg leading-1.2 font-semibold">{{ index + 1 }}</span>
                 </div>
                 <div class="group-last/swiperTrail:hidden absolute top-1/2 -translate-y-1/2 border-t border-dashed border-primary w-[calc(100%+20px)]"></div>
             </div>
-            <div class="relative mt-6 lg:mt-7 min-h-85 flex flex-col gap-5 shadow-base ring-(1 inset #F6F6F6) p-5 text-fblack rounded-5 bg-white">
+            <div class="relative mt-6 lg:mt-7 min-h-85 flex flex-col gap-5 shadow-base ring-(1 inset #F6F6F6) p-5 text-fblack rounded-5 bg-white grow">
                 <svg class="-z-1 absolute left-4.5 translate-x-1/2 translate-y-1 bottom-full" xmlns="http://www.w3.org/2000/svg" width="16" height="14" viewBox="0 0 16 14" fill="none">
                     <path d="M7.13397 0.5C7.51887 -0.166666 8.48112 -0.166667 8.86602 0.5L15.7942 12.5C16.1791 13.1667 15.698 14 14.9282 14H1.0718C0.301996 14 -0.179129 13.1667 0.205771 12.5L7.13397 0.5Z" fill="#fff" stroke="#F6F6F6" />
                 </svg>
