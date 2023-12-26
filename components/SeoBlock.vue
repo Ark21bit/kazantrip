2862f9435ec2577d01fcdbabe3986785596fbe17
Ark21bit <webleif@vk.com>
2023-12-26 16:39:53 +0300

    seoblock fix

--- components/SeoBlock.vue
+++ components/SeoBlock.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col gap-5 gap-7.5">
         <div v-for="item in isShort ? dataShorts : data" class="flex flex-col gap-5 lg:gap-7.5">
-            <h3 class="text-5.5 lg:text-2xl font-semibold font-Montserrat leading-1.2 lg:leading-1.2 text-fblack">{{ item?.lang_info?.title }}</h3>
-            <p class="text-sm lg:text-base leading-1.4 lg:leading-1.4 text-fblack">{{ item?.lang_info?.text }}</p>
+            <div class="text-5.5 lg:text-2xl font-semibold font-Montserrat leading-1.2 lg:leading-1.2 text-fblack" v-html="item?.lang_info?.title"></div>
+            <div class="text-sm lg:text-base leading-1.4 lg:leading-1.4 text-fblack" v-html="item?.lang_info?.text"></div>
         </div>
         <Button v-if="Number(data?.length) > 1" class="w-fit" size="lg" @click="toggle" padded variant="link">{{ isShort ? generalConfig?.static_info?.global_words?.more : generalConfig?.static_info?.global_words?.hide_more_details }}</Button>
     </div>
