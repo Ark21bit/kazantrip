b5e6751e6ac730a12063a57c7a598e8feb36979a
Ark21bit <webleif@vk.com>
2023-12-26 12:06:50 +0300

    modal confirmation fix

--- components/Modal/ModalConfirmation.vue
+++ components/Modal/ModalConfirmation.vue
@@ -1,7 +1,7 @@
 <template>
-    <Modal size="md" @close="closeModal" class="h-90 flex flex-col gap-6" :is-show="isModalShow">
+    <Modal size="md" @close="closeModal" class="min-h-60 lg:min-h-80 flex flex-col gap-6" :is-show="isModalShow">
         <img class="w-49 h-24 mt-2.5 mx-auto" src="/thanksBg.png" alt="">
-        <p class="text-base lg:text-2xl text-fblack leading-tight lg:leading-tight text-center">Проверьте внесённые данные и подтвердите заказ</p>
+        <p class="text-xl lg:text-2xl text-fblack leading-tight lg:leading-tight text-center">Проверьте внесённые данные и подтвердите заказ</p>
         <Button @click="closeModal()" class="mt-auto mx-auto w-full lg:w-49" variant="outline" color="green">{{ generalConfig?.static_info?.global_words?.close }}</Button>
     </Modal>
 </template>
