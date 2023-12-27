55c476728324dd4226d28b73aa3ca51012186e9b
Ark21bit <webleif@vk.com>
2023-12-27 15:48:09 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,13 @@
     <IndexProducts class="mt-15 lg:mt-20" :title="pageInfo?.content?.main_products?.title" :products="pageInfo?.main_products?.data" />
 
     <Feedback :subTitle="pageInfo?.content?.form_search?.description" :title="pageInfo?.content?.form_search?.title" />
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mt-15 lg:mt-37.5">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-[repeat(auto-fit,minmax(0,1fr))] gap-5 mt-15 lg:mt-37.5">
         <div class="flex flex-col gap-3 min-h-45.25 lg:min-h-48.75 p-5 lg:p-6 pr-20 border-(#F6F6F6 1) bg-[position:top_0_right_15px] lg:bg-[position:top_0_right_30px] bg-[length:48px] lg:bg-[length:51px] bg-[url(/imgs/pattern.svg)] bg-repeat-y rounded-5">
             <h3 class="text-fblack text-xl lg:text-6.5 font-Montserrat font-bold leading-1.2 lg:leading-1.2 [&>span]:text-#FF3D15" v-html="generalConfig?.static_info?.global_words?.yandex_review"></h3>
             <p class="text-second text-sm leading-1.4">{{ generalConfig?.static_info?.global_words?.read_reviews_our_clients }}</p>
             <Button :tag="CustomLink" to="https://yandex.ru/profile/1020776306" target="_blank" size="sm" class="w-41 mt-auto">{{ generalConfig?.static_info?.global_words?.more }}</Button>
         </div>
-        <div class="flex relative flex-col gap-3 min-h-45.25 lg:min-h-48.75 p-5 lg:p-6 border-(#F6F6F6 1) bg-black/50 rounded-5 overflow-hidden">
+        <div v-if="pageInfo?.main_is_exclusive" class="flex relative flex-col gap-3 min-h-45.25 lg:min-h-48.75 p-5 lg:p-6 border-(#F6F6F6 1) bg-black/50 rounded-5 overflow-hidden">
             <h3 class="text-white text-lg lg:text-6.5 font-Montserrat font-bold leading-1.2 lg:leading-1.2">{{ pageInfo?.main_is_exclusive?.lang_info?.title }}</h3>
             <Button :tag="CustomLink" :to="pageInfo?.main_is_exclusive?.slug" size="sm" class="w-41 mt-auto" color="yellow">{{ generalConfig?.static_info?.global_words?.more }}</Button>
             <div class="absolute h-full w-full -z-1 top-0 left-0 [&>img]:(object-cover object-center w-full h-full)" v-html="pageInfo?.main_is_exclusive?.media_preview"></div>
