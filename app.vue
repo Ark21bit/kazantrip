8233a81e34d4c76f2366d8eeea78dab438059684
Ark21bit <webleif@vk.com>
2024-01-11 18:08:56 +0300

    socials bloack fix && up button fix

--- app.vue
+++ app.vue
@@ -7,21 +7,21 @@
     <CustomLoadingIndicator />
     <div id="teleported"></div>
     <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" enter-active-class="transition-opacity duration-300 ease-in-out" leave-active-class="transition-opacity duration-300 ease-in">
-        <button v-show="scrollToUpButtonVisible" @click="scrollToUp" class="text-primary fixed top-1/2 -translate-y-1/2 right-[--width-content-margin] z-30 flex items-center gap-2.5 text-sm leading-1.25 group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
-            <span class="max-lg:hidden">{{ generalConfig?.static_info?.global_words?.up }}</span>
+        <button v-show="scrollToUpButtonVisible" @click="scrollToUp" class="text-primary fixed top-1/2 -translate-y-1/2 right-[--width-content-margin] z-30 flex flex-col items-center gap-2.5 text-sm leading-1.25 group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
             <span class="p-2.5 bg-#39919A rounded-full">
                 <span class="text-6 text-white i-custom:arrow-up"></span>
             </span>
+            <span class="max-lg:hidden">{{ generalConfig?.static_info?.global_words?.up }}</span>
         </button>
     </Transition>
-    <div class="fixed z-30 right-[--width-content-margin] bottom-7.5 leading-none items-end flex flex-col gap-2 text-white group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0">
-        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.telegram" class="p-2.5 bg-#2AABEE transition-colors duration-500 ease-linear rounded-lg">
+    <div class="fixed z-30 right-[--width-content-margin] bottom-7.5 leading-none items-end flex flex-col gap-2 text-white group-[.modal-open]/body:pr-4.25 group-[.modal-open.modal-open-mobile]/body:pr-0 pointer-events-none">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.telegram" class="p-2.5 bg-#2AABEE transition-colors duration-500 ease-linear rounded-lg pointer-events-auto">
             <span class="i-custom:tg text-2xl"></span>
         </CustomLink>
-        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.whatsApp" class="p-2.75 bg-#60D669 transition-colors duration-500 ease-linear rounded-lg">
+        <CustomLink target="_blank" :to="generalConfig?.static_info?.contact?.socials?.whatsApp" class="p-2.75 bg-#60D669 transition-colors duration-500 ease-linear rounded-lg pointer-events-auto">
             <span class="text-5.5 i-custom:whatsapp"></span>
         </CustomLink>
-        <CustomLink to="/excursions" class="py-3 px-4.125 bg-primary transition-colors duration-500 ease-linear rounded-lg text-base font-medium">
+        <CustomLink to="/excursions" class="py-3 px-4.125 bg-primary transition-colors duration-500 ease-linear rounded-lg pointer-events-auto text-base font-medium">
             {{ generalConfig?.static_info?.global_words?.all_excursion }}
         </CustomLink>
     </div>
