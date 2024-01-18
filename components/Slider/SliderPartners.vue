2ff94cdbad7f3170767e70864d200f8f7fe1c6bd
Ark21bit <webleif@vk.com>
2024-01-18 13:24:55 +0300

    slider partners fix

--- components/Slider/SliderPartners.vue
+++ components/Slider/SliderPartners.vue
@@ -1,9 +1,9 @@
 <template>
     <Swiper slides-per-view="auto" class="w-full flex relative contain-layout overflow-visible" v-bind="options">
-        <SwiperSlide v-for="slide in slides" class="w-33.75 h-33.75 lg:h-49.25 lg:w-49.25 last:mr-0 mr-5 rounded-5 border-#F6F6F6 shadow-base">
-            <div class="w-full h-23.75 lg:h-34.5 [&>img]:(h-full w-full object-contain object-center)" v-html="slide?.media_preview"></div>
-            <p class="py-3 lg:py-5 text-#7B7B7B text-sm leading-1.25 text-center">{{ slide?.lang_info?.title }}</p>
-            <CustomLink v-if="slide?.lang_info?.url" :to="slide?.lang_info?.url" target="_blank" class="absolute top-0 left-0 h-full w-full"></CustomLink>
+        <SwiperSlide v-for="slide in slides" class="size-44 lg:size-52 last:mr-0 mr-5 rounded-5 border-#F6F6F6 shadow-base">
+            <div class="w-full h-22 lg:h-30 [&>img]:(h-full w-full object-contain object-center)" v-html="slide?.media_preview"></div>
+            <p class="p-3 lg:p-5 text-#7B7B7B text-sm leading-1.25 text-center">{{ slide?.lang_info?.title }}</p>
+            <CustomLink v-if="slide?.lang_info?.url" :to="slide?.lang_info?.url" target="_blank" class="text-sm lg:text-base absolute top-0 left-0 h-full w-full"></CustomLink>
         </SwiperSlide>
     </Swiper>
 </template>
