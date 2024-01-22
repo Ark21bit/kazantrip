908f8b9d527bce579388c941d65fbb3b951a4b24
Ark21bit <webleif@vk.com>
2024-01-22 16:15:18 +0300

    card catalog fix

--- components/Card/_Catalog.vue
+++ components/Card/_Catalog.vue
@@ -2,11 +2,11 @@
     <div class="flex flex-col rounded-5 overflow-hidden shadow-base">
         <div class="relative h-50 lg:h-62.5">
             <div class="h-full w-full block [&>img]:(w-full h-full object-cover object-center)" v-html="img ?? getDefaultImg()"></div>
-            <CustomLink class="absolute left-0 top-0 w-full h-full" :to="slug"></CustomLink>
             <div class="flex gap-2 flex-wrap absolute top-5 lg:top-4 inset-x-5 lg:inset-x-4 ">
                 <div v-if="isSale" class="bg-#DC3333 px-3 lg:px-4 py-1.5 lg:py-2.25 rounded-full font-medium leading-1.2 lg:leading-1.2 text-sm lg:text-base text-white">{{ generalConfig?.static_info?.global_words?.sale_text }}</div>
                 <div v-for="item in nameplates" class="bg-#DC3333 px-3 lg:px-4 py-1.5 lg:py-2.25 rounded-full font-medium leading-1.2 lg:leading-1.2 text-sm lg:text-base text-white">{{ item }}</div>
             </div>
+            <CustomLink class="absolute left-0 top-0 w-full h-full" :to="slug"></CustomLink>
         </div>
         <div class="grow rounded-t-5 -mt-5 lg:-mt-9 p-5 flex flex-col gap-5 bg-white relative ring ring-inset ring-#F6F6F6">
             <div class="flex flex-col gap-4 grow">
